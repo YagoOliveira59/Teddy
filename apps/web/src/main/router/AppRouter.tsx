c870219e6c45c717c45e0407351ad3eaceb86ae7
YagoOlivera59 <ygocontato@gmail.com>
2025-07-02 05:36:57 -0300

    update lockfile

--- apps/web/src/main/router/AppRouter.tsx
+++ apps/web/src/main/router/AppRouter.tsx
@@ -5,20 +5,24 @@ import { makeLoginPage } from "@/main/factories/pages/login-page-factory";
 import { makeHomePage } from "@/main/factories/pages/home-page-factory";
 import { makeClientsPage } from "@/main/factories/pages/clients-page-factory";
 import { makeSelectedClientsPage } from "@/main/factories/pages/selected-clients-page-factory";
+import { ProtectedRoute } from "@/presentation/contexts/ProtectedRoute ";
 
 export function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
-
         <Route path="/login" element={makeLoginPage()} />
 
-        <Route element={<MainLayout />}>
-          <Route path="/home" element={makeHomePage()} />
-          <Route path="/clients" element={makeClientsPage()} />
-          <Route path="/selected-clients" element={makeSelectedClientsPage()} />
-
-          <Route path="/" element={<Navigate to="/home" replace />} />
+        <Route element={<ProtectedRoute />}>
+          <Route element={<MainLayout />}>
+            <Route path="/home" element={makeHomePage()} />
+            <Route path="/clients" element={makeClientsPage()} />
+            <Route
+              path="/selected-clients"
+              element={makeSelectedClientsPage()}
+            />
+            <Route path="/" element={<Navigate to="/home" replace />} />
+          </Route>
         </Route>
 
         <Route path="*" element={<Navigate to="/home" replace />} />
